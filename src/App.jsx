845a3d0fe5a591acb0fc1badf8e8f9a39253f719
bigm <you@example.com>
2023-08-03 04:33:32 +0800

    first

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter >
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
